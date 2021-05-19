29bf3a31bf4fe85ac5f952db89017efba2305b84
Jason Gong <jasongong83@gmail.com>
2021-05-18 20:54:37 -0400

    edit naming in token contract

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -8,7 +8,7 @@ contract Token is ERC20 {
 
   event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Jbank", "JB") {
+  constructor() public payable ERC20("Decentralized Bank Currency", "DBC") {
     minter = msg.sender;
   }
 
